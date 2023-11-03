39439e49c91f4b86b9549e06742d841cc2750787
Ratzeeh [DP] <dennis.poltorazki@gmail.com>
2023-11-03 16:18:03 +0100

    hpbar aber nicht so wie gewollt

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    public Image hpbar;
+    private bool dealingDmg = false;
+    private float dmgAmount = 0.2F;
+    private float dmgBound;
+    public float waitTime = 1.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         hpbar.fillAmount = 1.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dealingDmg)
+        {
+            if (dmgBound == hpbar.fillAmount)
+            {
+                dealingDmg = !dealingDmg;
+            }
+
+            hpbar.fillAmount -= dmgAmount / waitTime * Time.deltaTime;
+        }
+    }
+
+    public void TaskOnClick()
+    {
+        dealingDmg = !dealingDmg;
+        dmgBound = hpbar.fillAmount - dmgAmount;
+    }
+}
